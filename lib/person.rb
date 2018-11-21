12682498053f50faf55b02ef015c42c3f48231f8
kevmsutton <kevmsutton@gmail.com>
2018-11-21 16:40:57 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+require "pry"
+
+class Person
+
+  def name
+    @name
+  end
+  binding.pry
+
+  def name=(name)
+    @name = name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job)
+    @job = job
+   end
+
+end
